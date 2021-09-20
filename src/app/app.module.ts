98a94507df50da05d42be0af4c8bbf21c908f914
Artem Nikolaev <artem@nikolaev.by>
2021-09-20 20:28:39 +0300

    Z1: Practicing Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServersComponent } from './servers/servers.component';
+import { MyOwnComponentComponent } from './my-own-component/my-own-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    MyOwnComponentComponent,
   ],
   imports: [
     BrowserModule,
