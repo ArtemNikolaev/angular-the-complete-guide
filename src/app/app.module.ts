a4e56a2037bd1dff9ddcfd6907d998651b692169
Artem Nikolaev <artem@nikolaev.by>
2021-09-22 20:21:58 +0300

    47. Setting up the Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ServersComponent } from './servers/servers.component';
-import { MyOwnComponentComponent } from './my-own-component/my-own-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    MyOwnComponentComponent,
   ],
   imports: [
     BrowserModule,
