c2615a804677d25ffafe7c5107e50a946c40c7ed
Artem Nikolaev <artem@nikolaev.by>
2021-09-23 21:49:47 +0300

    52. Creating a "Recipe" Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipiesComponent } from './recipies/recipies.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { RecipiesComponent } from './recipies/recipies.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecipeService {
+  public name: string;
+  public description: string;
+  public imagePath: string;
 
-  constructor() { }
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
 }
