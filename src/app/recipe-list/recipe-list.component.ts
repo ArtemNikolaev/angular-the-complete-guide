d1564bf74319b566636ded96597991534f76ab7f
Artem Nikolaev <artem@nikolaev.by>
2021-09-23 21:56:55 +0300

    52. Creating a "Recipe" Model

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: RecipeService[] = [
+    new RecipeService('A Test Recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/RFO-LargeHero-1400x919-ChorizoChicken-mini-dc9a0314-51b5-4ae5-a8e8-77b8dee768e6-0-1400x920.jpg')
+  ];
 
   constructor() { }
 
