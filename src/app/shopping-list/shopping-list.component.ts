338823067eae436795145f0f646702c75f8c104d
Artem Nikolaev <artem@nikolaev.by>
2021-09-25 20:15:50 +0300

    89. Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,28 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changeIngridients(changedIngridient) {
+    const index = this.ingridients.findIndex((el) => el.name === changedIngridient.name);
+    console.log(index);
+
+    if (index === -1) {
+      if (changedIngridient.amount <= 0) return;
+
+      this.ingridients.push(new Ingridient(changedIngridient.name, changedIngridient.amount));
+
+      return;
+    }
+
+    if (this.ingridients[index].amount + changedIngridient.amount <= 0) {
+      this.ingridients.splice(index, 1);
+
+      return;
+    }
+
+    this.ingridients[index].amount += changedIngridient.amount;
+  }
+
+  clear() {
+    this.ingridients = [];
+  }
 }
